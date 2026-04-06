a341e7721d30a094f957e4030aee06c68f71c607
Abhi Satwika Reddy <abhisathvika1234@gmail.com>
2026-04-06 11:12:36 +0530

    code

---
+++ Day-5/Pattern1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Pattern1 {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        
+        for(int row=0; row<n; row++)
+        {
+            for(int col=0; col<n; col++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
